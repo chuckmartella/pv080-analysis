c0c478cc7d6ed2134bdca23f06b60e9d5dd0a9aa
Jan Zatloukal <xzatlouk@fi.muni.cz>
2022-04-27 16:54:44 +0200

    fix injection

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -3,6 +3,7 @@
 # https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import subprocess
 import base64
+import importlib
 import cPickle
 import flask
 
@@ -24,7 +25,9 @@ class RunBinSh():
         return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
-    exec("import urllib%s as urllib" % version)
+    # exec("import urllib%s as urllib" % version)
+    global urllib
+    urllib = importlib.import_module(f"urllib{version}")
 
 @app.route('/')
 def index():
